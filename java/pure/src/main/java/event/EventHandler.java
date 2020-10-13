11eff72dc0724251ab9124e2dac068600b6b9625
Swington <tomasz.switon@gmail.com>
2020-10-13 09:48:26 +0200

    Make pure java throw test exception when called

--- java/pure/src/main/java/event/EventHandler.java
+++ java/pure/src/main/java/event/EventHandler.java
@@ -1,7 +1,8 @@
 package event;
 
 public class EventHandler {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello, world! Once again!");
+        throw new Exception("Test exception");
     }
 }
