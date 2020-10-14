40a9a6cb6720bfc92674bf556729032f816693a2
Swington <tomasz.switon@gmail.com>
2020-10-14 11:54:41 +0200

    MVP-1209 Fix string formatting

--- java/pure/src/main/java/event/EventHandler.java
+++ java/pure/src/main/java/event/EventHandler.java
@@ -4,6 +4,6 @@ public class EventHandler {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, world! Once again!");
         String secrets = System.getProperty("secretParameters");
-        System.setProperty("output", String.format("{\"secrets\": \"%s\"}", secrets));
+        System.setProperty("output", String.format("{\"secrets\": %s}", secrets));
     }
 }
