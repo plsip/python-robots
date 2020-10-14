a18f996592cc28a13e208f78666391039e8bc458
Swington <tomasz.switon@gmail.com>
2020-10-14 11:43:04 +0200

    MVP-1209 Set output to print secrets

--- java/pure/src/main/java/event/EventHandler.java
+++ java/pure/src/main/java/event/EventHandler.java
@@ -3,6 +3,7 @@ package event;
 public class EventHandler {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, world! Once again!");
-        throw new Exception("Test exception");
+        String secrets = System.getProperty("secretParameters");
+        System.setProperty("output", String.format("{\"secrets\": \"%s\"}", secrets));
     }
 }
