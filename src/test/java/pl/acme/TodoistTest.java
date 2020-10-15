4a4705a8aa21636942be0c0a33f8d5ac3871f60a
Swington <tomasz.switon@gmail.com>
2020-10-15 19:02:01 +0200

    Move java tests to highest repo level

---
+++ src/test/java/pl/acme/TodoistTest.java
@@ -0,0 +1,35 @@
+package pl.acme;
+
+import org.junit.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TodoistTest {
+    @Test
+    public void verifyPageTitle() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--single-process");
+        options.addArguments("--use-gl=swiftshader");
+        options.addArguments("--no-zygote");
+
+        WebDriver browser = new ChromeDriver(options);
+        browser.get(System.getProperty("inputParameters.testTargetURL", "http://mir-todoist-test-app.s3-website-us-east-1.amazonaws.com/"));
+
+        String title = browser.getTitle();
+        System.out.println(title);
+
+        assertTrue((title.contentEquals("Inbox: Todoist")));
+
+        System.setProperty("output", "{\"title\":\"Inbox: Todoist\"}");
+
+        browser.close();
+    }
+}
