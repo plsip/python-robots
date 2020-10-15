22c888c2534d7199082a2226f16c31bc969d635e
Swington <tomasz.switon@gmail.com>
2020-10-15 10:44:53 +0200

    Add handling properties file to pure package

--- java/pure/src/main/java/event/EventHandler.java
+++ java/pure/src/main/java/event/EventHandler.java
@@ -1,9 +1,19 @@
 package event;
 
+import java.util.Properties;
+
 public class EventHandler {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, world! Once again!");
-        String secrets = System.getProperty("secretParameters");
+        loadProperties();
+        String secrets = System.getProperty("secret.parameters");
+        System.out.println(secrets);
         System.setProperty("output", String.format("{\"secrets\": %s}", secrets));
+    }
+
+    private static void loadProperties() throws Exception {
+        Properties properties = new Properties();
+        properties.load(EventHandler.class.getResourceAsStream("/myProperties.properties"));
+        System.setProperty("secret.parameters", properties.get("secret.parameters").toString());
     }
 }
