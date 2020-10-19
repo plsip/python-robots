6d3387168ae38fe36988e62123d5e0ffbb4865c6
Swington <tomasz.switon@gmail.com>
2020-10-19 14:34:51 +0200

    Add pandas to pure python task

--- python/pure/custom_lib.py
+++ python/pure/custom_lib.py
@@ -1,10 +1,17 @@
 import os
 
 import requests
+import pandas as pd
 
 def save_artifact():
     with open(os.path.join(os.environ['ARTIFACTS_PATH'], "example.txt"), 'w+') as f:
         f.write("Some artifact")
+        df = pd.DataFrame({
+            "Name": ["John", "Doe"],
+            "Age": [22, 23],
+            "Sex": ["Male", "NotSpecified"],
+        })
+        print(df)
     return requests.get("http://google.com")
 
 
