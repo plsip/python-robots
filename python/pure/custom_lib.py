975bf866893ebe282283eddd22876aa9ba8d0b56
Swington <tomasz.switon@gmail.com>
2020-10-19 14:07:56 +0200

    Use lib specified in rewquirements file

--- python/pure/custom_lib.py
+++ python/pure/custom_lib.py
@@ -1,8 +1,12 @@
 import os
 
+import requests
+
 def save_artifact():
     with open(os.environ['ARTIFACTS_PATH'], 'r') as f:
         f.write("Some artifact")
+    return requests.get("http://google.com")
+
 
 def save_printscreen():
     with open(os.environ['ARTIFACTS_PATH'], 'r') as f:
