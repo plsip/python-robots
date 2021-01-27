9dfec4499ac94ba943ce8cec4fd6d669365993f4
Pawel Lukasik <pawel.lukasik@makeitright.ia>
2021-01-27 14:23:17 +0100

    return array

--- python/robots/returns/main.py
+++ python/robots/returns/main.py
@@ -6,10 +6,10 @@ logger.setLevel(logging.INFO)
 
 def handler(event):
     logger.info("Received event: {}".format(event))
-    output = {
-        'out1': 'val1',
-        'out2': 'val2',
-    }
+    output = [
+        {'out1': 'val1'},
+        {'out2': 'val2'}
+    ]
     logger.info("Returning response: {}".format(output))
     return output
 
